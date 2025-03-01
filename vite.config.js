c50803a84973eac81da194de4348e469129acd73
Mfdal <maadimfdal@gmail.com>
2025-03-01 14:26:26 +0100

    Updated build target

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  build: {
+    target: "esnext", // Or 'modules', 'es2015', etc.
+    outDir: "dist", // Ensures output goes to the correct directory
+    sourcemap: true, // Helps debug issues
+  },
+  server: {
+    port: 3000, // Optional: Change dev server port
+  },
+});
