1e3d7cbb34b3381604a075b62717ebec032b19bc
Mfdal <maadimfdal@gmail.com>
2025-03-01 14:32:43 +0100

    logo added

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from "./project1/App";
 import Store from "./project1/redux/Store";
 import { Provider } from 'react-redux';
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
